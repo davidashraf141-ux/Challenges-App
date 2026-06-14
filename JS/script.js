921846ffba52f041c143260c606270e1349baaea
David Ashraf <davidashraf141@gmail.com>
2026-06-14 13:34:24 +0300

    Update1

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+
+const soloModeButton = document.getElementById("soloMode");
+const duoModeButton = document.getElementById("duoMode");
+
+
+soloModeButton.onclick = ()=>{
+    location.href = "solo.html";
+}
+
+duoModeButton.onclick = ()=>{
+    location.href = "duo.html";
+}
